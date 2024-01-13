f109ae4230c48a42fe35550d58038c5faddf9a76
Mohamed Siraj <siraj@oganro.com>
2024-01-13 23:50:36 +0530

    changes update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
-import Route from './routes';
+dotenv.config({path: `.env.${process.env.NODE_ENV}`});
+import route from './routes';
 
-/**
- * server configuration
- */ 
+/** 
+ * server configuration 
+ */
 const app = express();
 /**
  * route connect
  */
-app.use(Route);
+app.use(route);
 
 app.listen(process.env.PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${process.env.PORT}`);
